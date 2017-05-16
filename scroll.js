faa6c7631c1defde318680f852b0a522934bca36
Polarpi <eugeniachen123@gmail.com>
2017-05-16 01:28:33 -0500

    Website in progress

---
+++ scroll.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scroll-button").style.display = "block";
+    } else {
+        document.getElementById("scroll-button").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
